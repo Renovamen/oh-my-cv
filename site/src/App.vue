54ca3ebf1e0c5bfd1e963df1c00b193825cec996
邹笑寒 <renovamenzxh@gmail.com>
2022-08-22 00:45:14 -0400

    fix: meta description

--- site/src/App.vue
+++ site/src/App.vue
@@ -5,13 +5,15 @@
 <script setup lang="ts">
 const { t, locale } = useI18n();
 
+const desc = computed(() => t("desc"));
+
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
   meta: [
-    { name: "description", content: t("desc") },
-    { property: "og:description", content: t("desc") },
+    { name: "description", content: desc },
+    { property: "og:description", content: desc },
     { property: "og:locale", content: locale },
     {
       name: "theme-color",
