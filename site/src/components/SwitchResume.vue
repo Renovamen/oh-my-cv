8895ded805bfe736363dba262d5a5aca464c1e57
邹笑寒 <renovamenzxh@gmail.com>
2022-09-02 21:51:22 -0400

    fix: date display

--- site/src/components/SwitchResume.vue
+++ site/src/components/SwitchResume.vue
@@ -59,7 +59,7 @@ const getDateStr = (timestamp: string): string => {
 
   const y = date.getFullYear();
   const m = (date.getMonth() + 1).toString().padStart(2, "0");
-  const d = date.getDate();
+  const d = date.getDate().toString().padStart(2, "0");
 
   return y + "-" + m + "-" + d;
 };
