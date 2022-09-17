01bb23c74eebe2a166d7ed9e372b60e60f14ea3a
邹笑寒 <renovamenzxh@gmail.com>
2022-09-17 01:13:14 -0400

    feat(utils): add slugify

--- packages/utils/src/common.ts
+++ packages/utils/src/common.ts
@@ -9,4 +9,19 @@ export const isExternal = (path: string) => {
   return outboundRE.test(path);
 };
 
+// https://github.com/meteorlxy/vscode-slugify
+export const slugify = (str: string) =>
+  encodeURI(
+    str
+      .trim()
+      .toLowerCase()
+      .replace(/\s+/g, "-") // Replace whitespace with -
+      .replace(
+        /[\]\[\!\'\#\$\%\&\(\)\*\+\,\.\/\:\;\<\=\>\?\@\\\^\_\{\|\}\~\`。，、；：？！…—·ˉ¨‘’“”々～‖∶＂＇｀｜〃〔〕〈〉《》「」『』．〖〗【】（）［］｛｝]/g,
+        ""
+      ) // Remove known punctuators
+      .replace(/^\-+/, "") // Remove leading -
+      .replace(/\-+$/, "") // Remove trailing -
+  );
+
 export const copy = (obj: any) => JSON.parse(JSON.stringify(obj));
