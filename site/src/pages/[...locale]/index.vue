6adf1c93bbd087bf97bfee9b47da68a47d63b834
邹笑寒 <renovamenzxh@gmail.com>
2023-02-22 21:26:55 +0800

    fix: mobile landing page

--- site/src/pages/[...locale]/index.vue
+++ site/src/pages/[...locale]/index.vue
@@ -4,16 +4,16 @@
 
     <div class="workspace">
       <div
-        class="absolute px-5 md:px-20 lt-sm:pb-10 w-fit h-fit max-w-240 left-0 right-0 top-0 bottom-0 m-auto"
+        class="px-5 md:px-20 pt-40 h-fit max-w-240 lt-sm:pb-10 sm:(absolute left-0 right-0 top-0 bottom-0 m-auto)"
       >
-        <div text-center mt-40>
+        <div text-center>
           <h1 text="c 3xl sm:4xl" v-html="t('landing.hero')" />
           <div my-10 text-light-c sm:text-lg>
             <BrandName /> {{ t("landing.desc") }}
           </div>
           <router-link :to="switchPath('resumes', locale)">
             <span
-              class="text-white bg-brand px-4 py-3 rounded-lg mx-auto outline outline-4 outline-transparent duration-200 hover:(outline-rose-300/50)"
+              class="text-white bg-brand px-4 py-3 rounded-lg mx-auto outline outline-4 outline-transparent duration-200 hover:outline-rose-300/50"
             >
               {{ t("landing.start") }}
             </span>
