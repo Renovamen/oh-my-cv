380f943fefd66c1bd26d9d9d74f7c327975fba75
邹笑寒 <renovamenzxh@gmail.com>
2023-11-15 13:44:35 -0500

    chore: bump deps

--- site/src/app.vue
+++ site/src/app.vue
@@ -24,7 +24,7 @@ useHead({
     { property: "og:locale", content: locale },
     {
       name: "theme-color",
-      content: () => (colorMode.preference === "dark" ? "#475569" : "#f3f4f6")
+      content: () => (colorMode?.preference === "dark" ? "#475569" : "#f3f4f6")
     }
   ],
   link: [
--- site/src/components/shared/ToggleDark.vue
+++ site/src/components/shared/ToggleDark.vue
@@ -1,22 +1,18 @@
 <template>
-  <button class="round-btn" :title="$t('toggle_dark')" @click="toggleDark()">
-    <div v-show="colorMode.preference === 'dark'" flex-center>
+  <button
+    class="round-btn"
+    :title="$t('toggle_dark')"
+    @click="$colorMode.preference = $colorMode.preference === 'dark' ? 'light' : 'dark'"
+  >
+    <div v-show="$colorMode.preference === 'dark'" flex-center>
       <client-only>
         <span class="iconify md:text-lg" data-icon="ph:moon-bold" />
       </client-only>
     </div>
-    <div v-show="colorMode.preference !== 'dark'" flex-center>
+    <div v-show="$colorMode.preference !== 'dark'" flex-center>
       <client-only>
         <span class="iconify md:text-lg" data-icon="ph:sun-bold" />
       </client-only>
     </div>
   </button>
 </template>
-
-<script setup lang="ts">
-const colorMode = useColorMode();
-
-const toggleDark = () => {
-  colorMode.preference = colorMode.preference === "dark" ? "light" : "dark";
-};
-</script>
