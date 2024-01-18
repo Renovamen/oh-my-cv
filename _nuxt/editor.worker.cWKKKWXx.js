9c5936dcefcd330b96efebe520e8164f752c3d55
Renovamen <Renovamen@users.noreply.github.com>
2024-01-18 05:56:13 +0000

    deploy: b008bc73d37a12e0ffb9a99994841e3f1c6d632c

--- _nuxt/editor.worker.cWKKKWXx.js
+++
@@ -1,1 +0,0 @@
-function e(r){return new Worker(""+new URL("editor.worker-R6RRFAyK.js",import.meta.url).href,{name:r==null?void 0:r.name})}export{e as default};
---
+++ _nuxt/editor.worker.t1F1ytmT.js
@@ -0,0 +1,1 @@
+function r(e){return new Worker(""+new URL("editor.worker-weymZOpQ.js",import.meta.url).href,{name:e==null?void 0:e.name})}export{r as default};
