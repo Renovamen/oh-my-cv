afb174fa903f552fd427b51e5effe83755fd9fc1
邹笑寒 <renovamenzxh@gmail.com>
2024-07-01 22:38:11 -0400

    feat: automatic theme change based on system preferences

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -37,7 +37,6 @@ export default defineNuxtConfig({
   },
 
   colorMode: {
-    preference: "light",
     classSuffix: ""
   },
 
--- site/src/components/shared/ToggleDark.vue
+++ site/src/components/shared/ToggleDark.vue
@@ -3,13 +3,55 @@
     variant="ghost-secondary"
     size="round"
     :aria-label="$t('toggle_dark')"
-    @click="$colorMode.preference = $colorMode.preference === 'dark' ? 'light' : 'dark'"
+    @click="switchMode"
   >
     <div
-      class="i-ph:sun-bold size-4 scale-100 rotate-0 dark:(scale-0 -rotate-90) transition-transform duration-500"
-    />
-    <div
-      class="i-ph:moon-bold size-4 absolute scale-0 rotate-90 dark:(scale-100 rotate-0) transition-transform duration-500"
+      v-for="(mode, i) in modes"
+      :class="[
+        'absolute transition-transform duration-500',
+        mode.icon,
+        transform(mode.value)
+      ]"
     />
   </UiButton>
 </template>
+
+<script lang="ts" setup>
+type ModeValue = "light" | "dark" | "system";
+
+const modes: Array<{ value: ModeValue; icon: string }> = [
+  {
+    value: "system",
+    icon: "i-material-symbols:night-sight-auto-rounded size-4.5"
+  },
+  {
+    value: "light",
+    icon: "i-ph:sun-bold size-4"
+  },
+  {
+    value: "dark",
+    icon: "i-ph:moon-bold size-4"
+  }
+];
+
+const colorMode = useColorMode();
+
+const _findModeIndex = (mode: string) => modes.findIndex((m) => m.value === mode);
+
+const _findNeighbourMode = (mode: string, position: -1 | 1) => {
+  const index = _findModeIndex(mode);
+  return modes[(modes.length + index + position) % modes.length].value;
+};
+
+const switchMode = () => {
+  colorMode.preference = _findNeighbourMode(colorMode.preference, 1);
+};
+
+const transform = (mode: ModeValue) => {
+  return colorMode.preference === mode
+    ? "scale-100 rotate-0"
+    : colorMode.preference === _findNeighbourMode(mode, -1)
+      ? "scale-0 rotate-90"
+      : "scale-0 -rotate-90";
+};
+</script>
