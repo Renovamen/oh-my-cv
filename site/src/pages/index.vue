f3beee2f754c54b6ab3fee677afa8a43dba9a79a
邹笑寒 <renovamenzxh@gmail.com>
2024-07-02 19:44:30 -0400

    chore: tweak landing page

--- site/src/pages/index.vue
+++ site/src/pages/index.vue
@@ -4,11 +4,11 @@
 
     <div
       class="h-fit max-w-240 sm:(absolute inset-0 m-auto)"
-      p="x-5 t-40 md:x-20 lt-sm:b-10"
+      p="x-5 t-30 md:x-20 lt-sm:b-10"
     >
       <div text-center>
         <h1 text="3xl sm:4xl" v-html="$t('landing.hero')" />
-        <div my-10 sm:text-lg><SharedBrandName /> {{ $t("landing.desc") }}</div>
+        <div my-10 text="lg sm:xl"><SharedBrandName /> {{ $t("landing.desc") }}</div>
 
         <UiButton
           :as="NuxtLink"
